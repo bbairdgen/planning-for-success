b240baaca71e1c64572335a3a7c5e6c1e9f88d3c
bbairdgen <bbairdgen@gmail.com>
2023-01-16 21:51:30 -0700

    adding javascript file

--- script.js
+++ script.js
@@ -2,31 +2,46 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var timeDisplayEl = $('#currentDay')
+var descriptionEl = $('.description')
+var saveButton = $('.saveBtn')
+
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  function displayDate() {
+
+  saveButton.on('click', function () {
+    var storedEvents = $(this).siblings('textarea').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, storedEvents)
+  })
+
+
+  $('textarea').each(function () {
+    // add variables comparing now(dayjs) to parseInt() if-else statements 
+    var now = dayjs()
+    var current = parseInt($(this).attr('id'));
+
+    if (current > now) {
+      $(this).addClass('future') 
+      $(this).addClass('past')
+    } else {
+      $(this).addClass('present')
+    }
+  })
+
+  $('textarea').each(function() {
+      var retrieve = $(this).parent().attr('id');
+
+      $(this).val(localStorage.getItem(retrieve));
+    })
+  
+
+   function displayDate() {
     var today = dayjs().format('dddd, DD MMMM YYYY')
     timeDisplayEl.text(today)
   }
 
   displayDate()
-  
+  getItem()
+
 });
